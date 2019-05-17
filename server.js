9963de945ca379c963d4a2fc0b61c317ea384217
Nick Johnson <Blackjacknick@gmail.com>
2019-05-17 09:49:37 -0600

    trying to deploy

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const cors = require('cors');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 require('./db/db');
+const path = require('path');
 const store = new MongoDBStore({
     uri: 'mongodb://localhost:27017/bets',
     collection: 'mySessions'
   });
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(cors({
     origin: "http://localhost:3000",
     credentials: true,
@@ -31,9 +34,12 @@ app.use((req, res, next)=>{
     next();
 })
 
+
 const userController = require('./controllers/UserController');
 const betController = require('./controllers/Betcontroller');
-
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.use('/users', userController);
 app.use('/bets', betController);
